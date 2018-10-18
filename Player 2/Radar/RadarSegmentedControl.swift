36d22eb6789f6e60ce1ed39a472a263bafaba7df
Michele <michelemarrazzo93@gmail.com>
2018-10-18 21:09:20 +0200

    Static radar done! Tried to make random MIniPropic but no way...for now

--- Player 2/Radar/RadarSegmentedControl.swift
+++ Player 2/Radar/RadarSegmentedControl.swift
@@ -10,23 +10,15 @@ import UIKit
 
 class RadarSegmentedControl: UIViewController {
     
-    
 
     @IBOutlet var GamerRadar: UIView! = UIView()
+    @IBOutlet var GamerRadarSearched: UIView! = UIView()
     @IBOutlet var ClansRadar: UIView! = UIView()
     @IBOutlet var EventsRadar: UIView! = UIView()
     
     @IBOutlet weak var SegmentationRadar: UISegmentedControl!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        GamerRadar.isHidden = false
-        ClansRadar.isHidden = true
-        EventsRadar.isHidden = true
-        
-        // Do any additional setup after loading the view.
-    }
-    
+
     @IBAction func RadarChanger(_ sender: Any) {switch (SegmentationRadar.selectedSegmentIndex)
     {
     case 0:
@@ -52,9 +44,66 @@ class RadarSegmentedControl: UIViewController {
         }
     }
 
-
+    
+    @IBOutlet var GamerRadarView: UIView! = UIView()
+    
+    @IBOutlet weak var NavBarGamerRadar: UINavigationItem!
+    
+    @IBOutlet var Player1: UIView! = UIView()
+    
+    @IBAction func PropicButt(_ sender: Any) {
+        RandomViewMover(MiniPropic: Player1)
+    }
     
 
+
+    
+    func RandomViewMover(MiniPropic: UIView) {
+        
+        // Find the button's width and height
+        let buttonWidth = MiniPropic.frame.width
+        let buttonHeight = MiniPropic.frame.height
+        
+        // Find the width and height of the enclosing view
+        print(Player1.frame.width)
+        let viewWidth = GamerRadarView.bounds.width
+        let viewHeight = GamerRadarView.bounds.height
+        
+        // Compute width and height of the area to contain the button's center
+        let xwidth = viewWidth - buttonWidth
+        let yheight = viewHeight - buttonHeight
+        
+        // Generate a random x and y offset
+        let xoffset = CGFloat(arc4random_uniform(UInt32(xwidth)))
+        let yoffset = CGFloat(arc4random_uniform(UInt32(yheight)))
+        
+        // Offset the button's center by the random offsets.
+        MiniPropic.center.x = xoffset + buttonWidth / 2
+        MiniPropic.center.y = yoffset + buttonHeight / 2
+        print(Player1.frame.width)
+        print("here")
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        GamerRadar.isHidden = false
+        ClansRadar.isHidden = true
+        EventsRadar.isHidden = true
+        
+        self.navigationController?.isNavigationBarHidden = false //to hide navigation bar in radars views
+        
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController?.navigationBar.isTranslucent = true
+        navigationController?.navigationBar.tintColor = .red
+        self.navigationController?.navigationBar.tintColor = #colorLiteral(red: 1, green: 0.6635502577, blue: 0.009707250632, alpha: 1)
+        
+        //Make Navigation bar Trasparent
+        
+        // Do any additional setup after loading the view.
+    }
+    
     
 
     /*
